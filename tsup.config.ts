10cfe682747bf55c39667592111a3d98fe9744d5
set <lucameopou@gmail.com>
2025-05-11 13:48:01 +0200

    chore(tsup): add initial TypeScript build configuration

---
+++ tsup.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "tsup"
+
+export default defineConfig({
+  entry: ["src/**/*.ts", "!src/**/*.test.ts"],
+  splitting: false,
+  sourcemap: true,
+  clean: true,
+  minify: true,
+})
