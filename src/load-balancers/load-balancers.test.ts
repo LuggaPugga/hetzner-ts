2022d8ce96011ae5287d37937acaebaa05c17fb9
Luca <lucameopou@gmail.com>
2025-09-20 10:13:27 +0200

    test(load-balancers): add tests for creating and deleting load balancers

--- src/load-balancers/load-balancers.test.ts
+++ src/load-balancers/load-balancers.test.ts
@@ -8,6 +8,7 @@ function generateUniqueName(prefix: string = "") {
   return `${prefix}-${Math.random().toString(36).substring(2, 15)}`
 }
 
+let loadBalancerToDelete: number = 0
 test("list load balancers throws error if token is invalid", async () => {
   const result = await invalidHetzner.loadBalancers.getAll()
   expect(result.success).toBe(false)
@@ -22,6 +23,27 @@ test("list load balancers returns a list of load balancers", async () => {
 test("get load balancer throws error if ", async () => {
   const result = await workingHetzner.loadBalancers.get(999999999)
   expect(result.success).toBe(false)
+})
+
+test("create a load balancer and verify its properties", async () => {
+  const result = await workingHetzner.loadBalancers.create({
+    name: generateUniqueName("test-load-balancer"),
+    load_balancer_type: "lb11",
+    location: "fsn1",
+  })
+  if(result.success) {
+    loadBalancerToDelete = result.response.load_balancer.id
+  }
+  expect(result.success).toBe(true)
+  if (result.success) {
+    expect(result.response).toBeTypeOf("object")
+    expect(result.response.load_balancer.id).toBeTypeOf("number")
+  }
+})
+
+test("delete a load balancer", async () => {
+  const result = await workingHetzner.loadBalancers.delete(loadBalancerToDelete)
+  expect(result.success).toBe(true)
 })
 
 test("get load balancer returns a load balancer", async () => {
